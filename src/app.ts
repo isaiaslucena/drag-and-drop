204eb86d1fb4b199a8331876e34b3ed66a60159c
isaiasneto <isaiasneto@gmail.com>
2021-03-29 20:51:14 -0300

    Extended project item class

--- src/app.ts
+++ src/app.ts
@@ -125,6 +125,31 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
   abstract renderContent(): void;
 }
 
+class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
+  private project: Project;
+
+  get peopleOrPerson() {
+    return this.project.people === 1 ? 'person' : 'people';
+  }
+
+  constructor(hostId: string, project: Project) {
+    super('single-project', hostId, false, project.id);
+    this.project = project;
+
+    this.configure();
+    this.renderContent();
+  }
+
+  configure() {}
+
+  renderContent() {
+    this.element.querySelector('h2')!.textContent = this.project.title;
+    this.element.querySelector('h3')!.textContent = `${this.project.people.toString()} ${this.peopleOrPerson} assigned`;
+    this.element.querySelector('p')!.textContent = this.project.description;
+
+  }
+}
+
 class ProjectList extends Component<HTMLDivElement, HTMLElement> {
   assignedProjects: Project[];
 
@@ -160,9 +185,7 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> {
     const listElement = document.getElementById(`${this.type}-projects-list`)! as HTMLUListElement;
     listElement.innerHTML = '';
     for (const projectItem of this.assignedProjects) {
-      const listItem = document.createElement('li');
-      listItem.textContent = projectItem.title;
-      listElement.appendChild(listItem);
+      new ProjectItem(this.element.querySelector('ul')!.id, projectItem);
     }
   }
 }
