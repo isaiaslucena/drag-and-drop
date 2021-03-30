d559bbdca6834971c5abf28731481df38351ba89
isaiasneto <isaiasneto@gmail.com>
2021-03-30 12:49:58 -0300

    Created interfaces files

---
+++ src/interfaces/draggable.ts
@@ -0,0 +1,10 @@
+export interface Draggable {
+  dragStartHandler(event: DragEvent): void;
+  dragEndHandler(event: DragEvent): void;
+}
+
+export interface DraggableTarget {
+  dragOverHandler(event: DragEvent): void;
+  dropHandler(event: DragEvent): void;
+  dragLeaveHandler(event: DragEvent): void;
+}
