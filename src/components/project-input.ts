0c8f09c780830dc27efab4fe51334e5ebe995e3b
isaiasneto <isaiasneto@gmail.com>
2021-03-30 12:50:11 -0300

    Created components files

---
+++ src/components/project-input.ts
@@ -0,0 +1,79 @@
+import { Component } from './base-components.js';
+import { autoBind } from '../decorators/auto-bind.js';
+import { ProjectState } from '../state/project.js';
+import { validate } from '../util/validation.js';
+import { ValidateObject } from '../interfaces/validation.js';
+
+const projectState = ProjectState.getInstance();
+
+export class ProjectInput extends Component<HTMLDivElement, HTMLElement> {
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
+
+  constructor() {
+    super('project-input', 'app', true, 'user-input');
+    this.titleInputElement = this.element.querySelector('#title')! as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector('#description')! as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector('#people')! as HTMLInputElement;
+
+    this.configure();
+  }
+
+  configure() {
+    this.element.addEventListener('submit', this.submitHandler);
+  }
+
+  renderContent() { }
+
+  private gatherUserInput(): [string, string, number] | void {
+    const enteredTitle = this.titleInputElement.value;
+    const enteredDescription = this.descriptionInputElement.value;
+    const enteredPeople = this.peopleInputElement.value;
+
+    const validateTitleObject: ValidateObject = {
+      value: enteredTitle,
+      required: true,
+      minLength: 3
+    };
+
+    const validateDescriptionObject: ValidateObject = {
+      value: enteredDescription,
+      required: true,
+      minLength: 5
+    };
+
+    const validatePeopleObject: ValidateObject = {
+      value: +enteredPeople,
+      required: true,
+      min: 1,
+      max: 5
+    };
+
+    if (!validate(validateTitleObject) || !validate(validateDescriptionObject) || !validate(validatePeopleObject)) {
+      alert('Invalid input, please try again.');
+      return;
+    } else {
+      return [enteredTitle, enteredDescription, +enteredPeople];
+    }
+  }
+
+  private clearInputs() {
+    this.titleInputElement.value = '';
+    this.descriptionInputElement.value = '';
+    this.peopleInputElement.value = '';
+  }
+
+  @autoBind
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    const userInput = this.gatherUserInput();
+
+    if (Array.isArray(userInput)) {
+      const [title, description, people] = userInput;
+
+      projectState.addProject(title, description, people);
+      this.clearInputs();
+    }
+  }
+}
---
+++ src/components/project-list.ts
@@ -0,0 +1,72 @@
+import { Component } from './base-components.js';
+import { autoBind } from '../decorators/auto-bind.js';
+import { Project, ProjectStatus } from "../models/project.js";
+import { DraggableTarget } from "../interfaces/draggable.js";
+import { ProjectState } from '../state/project.js';
+import { ProjectItem } from '../components/project-item.js';
+
+const projectState = ProjectState.getInstance();
+
+export class ProjectList extends Component<HTMLDivElement, HTMLElement> implements DraggableTarget {
+  assignedProjects: Project[];
+
+  constructor(private type: 'active' | 'finished') {
+    super('project-list', 'app', false, `${type}-projects`);
+    this.assignedProjects = [];
+
+    this.configure();
+    this.renderContent();
+  }
+
+  @autoBind
+  dragOverHandler(event: DragEvent) {
+    if (event.dataTransfer && event.dataTransfer.types[0] === 'text/plain') {
+      event.preventDefault();
+      this.element.querySelector('ul')!.classList.add('droppable');
+    }
+  }
+
+  @autoBind
+  dropHandler(event: DragEvent) {
+    const projectId = event.dataTransfer!.getData('text/plain');
+    const projectStatus = this.type === 'active' ? ProjectStatus.Active : ProjectStatus.Finished;
+    projectState.moveProject(projectId, projectStatus);
+  }
+
+  @autoBind
+  dragLeaveHandler(_event: DragEvent) {
+    this.element.querySelector('ul')!.classList.remove('droppable');
+  }
+
+  configure() {
+    this.element.addEventListener('dragover', this.dragOverHandler);
+    this.element.addEventListener('drop', this.dropHandler);
+    this.element.addEventListener('dragleave', this.dragLeaveHandler);
+
+    projectState.addListener((projects: Project[]) => {
+      const relevantProjects = projects.filter((project) => {
+        if (this.type === 'active') {
+          return project.status === ProjectStatus.Active;
+        }
+
+        return project.status === ProjectStatus.Finished;
+      });
+      this.assignedProjects = relevantProjects;
+      this.renderProjects();
+    });
+  }
+
+  renderContent() {
+    const listId = `${this.type}-projects-list`;
+    this.element.querySelector('ul')!.id = listId;
+    this.element.querySelector('h2')!.textContent = `${this.type.toUpperCase()} PROJECTS`;
+  }
+
+  private renderProjects() {
+    const listElement = document.getElementById(`${this.type}-projects-list`)! as HTMLUListElement;
+    listElement.innerHTML = '';
+    for (const projectItem of this.assignedProjects) {
+      new ProjectItem(this.element.querySelector('ul')!.id, projectItem);
+    }
+  }
+}
